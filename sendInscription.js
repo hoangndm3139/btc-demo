66cbb97661efe2f546b78d6d8a9bb9514b451f72
HoangNDM6 <mhoang.9699@gmail.com>
2024-04-03 15:04:13 +0700

    init commit

---
+++ sendInscription.js
@@ -0,0 +1,38 @@
+import * as bitcoin from "bitcoinjs-lib";
+
+// Prev Transaction
+const input = {
+  hash: "", //txId
+  index: 1,
+  nonWitnessUtxo: Buffer.from("", "hex") //tx hex
+}
+
+const inputInscription = {
+    hash: "", //txId
+    index: 1,
+    nonWitnessUtxo: Buffer.from("", "hex") //tx hex
+  }
+
+// Add memo
+const data = Buffer.from('memo', 'utf8');
+const embed = bitcoin.payments.embed({ data: [data] });
+
+// Create psbt
+const psbt = new bitcoin.Psbt({ network: bitcoin.networks.testnet })
+  .addInput(input)
+  .addInput(inputInscription)
+  .addOutput({
+    script: embed.output,
+    value: 0,
+  })
+  .addOutput({
+    address: "", //receiving address
+    value: 99,
+  })
+  .addOutput({
+    address: "", //pay fee address
+    value: 99,
+  })
+
+// Generate psbt
+console.log(psbt.toHex());
